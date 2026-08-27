c800ca6ad32281cc9985b3bf3460af302a1dbc66
yy587 <1633793625@qq.com>
2026-08-27 12:19:24 +0800

    Replace contact map artwork

--- assets/contact-map-override.js
+++ assets/contact-map-override.js
@@ -1,4 +1,4 @@
-const mappingPath = "/assets/contact-campus-sketch-white-ecfcf96a.png";
+const mappingPath = "/assets/contact-campus-sketch-20260827.jpg";
 
 function replaceContactMapping() {
   document
